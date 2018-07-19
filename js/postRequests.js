71b9ec4389e8db376a1bff606377b14e55b5b5cf
Karun <karun@Karuns-MacBook-Pro.local>
2018-07-18 23:18:43 -0700

    updated html and css

--- js/postRequests.js
+++ js/postRequests.js
@@ -1,1 +1,7 @@
+/*File: postRequest.js
+  Description: Allows the user to send post requests to a backend json server
+  Author: Karun Sharma
+  Date 7-18-18
+  Version: 1.0
+*/
 var xhr = new XMLHttpRequest();
